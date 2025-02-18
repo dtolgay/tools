fa502db47388d3c0634cafb2484ae46125304241
Doga Tolgay <dtolgay@nia-login02.scinet.local>
2025-02-18 17:23:07 -0500

    Added dust2metal=0.4 ratio

--- constants.py
+++ constants.py
@@ -28,28 +28,34 @@ year2gigaYear                       = 1e-9              # 1 year -> 1 gigayear
 
 # Solar Metallicity information is taken from paper: 
 # SOLAR METALLICITY DERIVED FROM IN SITU SOLAR WIND COMPOSITION by R. von Steiger and T. H. Zurbuchen, 2015
-solar_metallicity 	= 0.02 			    # solar mass fraction
-OH_solar            = 4.89e-4           # Assuming 12 + log(O/H solar) = 8.69 Accurso+2017 -- Deriving a multivariate αCO conversion function using the [C II]/CO (1-0) ratio and its application to molecular gas scaling relations
-proton_mass         = 1.67262192e-27    # kg
-h                   = 4.1357e-15        # Plank's constant in eV s
-c                   = 299792458         # m/s 
-u_HAB               = 5.29e-15          # Habing energy density J/m^3
-G0                  = 1.69              # Habing radiation field Unitless
-w2ergs              = 1e7               # W -> erg/s
-Lsolar2ergs         = 3.826e33          # Lsolar -> erg/s. I think the value that I am using right now is true   # 3.839e33??? $ 3.85e33 in lequeux the interstellar medium
-ergs2Lsolar         = 1/Lsolar2ergs     # erg/s -> Lsolar
-ev2K                = 11606             # ev -> K
-year2seconds        = 3.154e7           # year -> seconds 
-kb                  = 1.380649e-23      # J/K  --- Boltzmann Constant
-gravitational_constant = 6.6743e-11     # N m^2 kg^-2
+solar_metallicity 	= 0.02 			        # solar mass fraction
+OH_solar            = 4.89e-4               # Assuming 12 + log(O/H solar) = 8.69 Accurso+2017 -- Deriving a multivariate αCO conversion function using the [C II]/CO (1-0) ratio and its application to molecular gas scaling relations
+proton_mass         = 1.67262192e-27        # kg
+proton_mass_gr      = proton_mass * kg2g    # gr
+h                   = 4.1357e-15            # Plank's constant in eV s
+c                   = 299792458             # m/s 
+u_HAB               = 5.29e-15              # Habing energy density J/m^3
+G0                  = 1.69                  # Habing radiation field Unitless
+w2ergs              = 1e7                   # W -> erg/s
+Lsolar2ergs         = 3.826e33              # Lsolar -> erg/s. I think the value that I am using right now is true  # 3.839e33??? $ 3.85e33 in lequeux the interstellar medium
+ergs2Lsolar         = 1/Lsolar2ergs         # erg/s -> Lsolar
+ev2K                = 11606                 # ev -> K
+year2seconds        = 3.154e7               # year -> seconds 
+kb                  = 1.380649e-23          # J/K  --- Boltzmann Constant
+gravitational_constant = 6.6743e-11         # N m^2 kg^-2
 
 mu_h                = 2.3e-24 / (proton_mass * kg2g)  # Effect of the helium atoms on the total mass ~1.37
+dust2metal          = 0.4                  # metallicity to dust ratio
 # From A COMPARISON OF METHODS FOR DETERMINING THE MOLECULAR CONTENT OF MODEL GALAXIES. Krumholz fh2 methadology paper
 
 
-# Constants for atomic data 
-mco_over_mh         = 28.01/1            # CO/H mass ratio
-mh2_over_mh         = 2.016/1            # H2/H mass ratio
+# Constants for atomic data
+avogadro_number     = 6.022e23            # Avogadro's number 
+mco_molecular_mass  = 28.01              # g/mol
+mh2_molecular_mass  = 2.016              # g/mol
+
+mco_gr              = mco_molecular_mass / avogadro_number  # g
+mh2_gr              = mh2_molecular_mass / avogadro_number  # g
 
 # Cosmology 
 eV2K                = 11606             # eV -> K
